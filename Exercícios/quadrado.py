3dd6c051105ed9f199de52332c845129925b243f
GabrielyB <gabrielybarbosa191@gmail.com>
2022-05-13 21:11:11 -0400

    mais exercícios

---
+++ Exercícios/quadrado.py
@@ -0,0 +1,8 @@
+from modulefinder import LOAD_CONST
+
+
+lado = input("Digite o valor correspondente ao lado de um quadrado: ")
+x = (float(lado))**2
+y = (float(lado))*4
+
+print("perímetro:",y, "- área:", x)
---
+++ opcionais/dezenas.py
@@ -0,0 +1,5 @@
+numero=input("Digite um número inteiro:")
+centena=(float(numero))//10
+dezena=centena%10
+
+print("O dígito das dezenas é",dezena)
